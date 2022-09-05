2e98036438f34b82a23c86d678bf1bd242209010
Tiago Pache <tiago.pache@bairesdev.com>
2022-09-05 14:06:56 -0300

    (wip) Adding i18n language switch using a dropdown interface (missing files)

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import i18n
 from dash import Dash
 from dash_bootstrap_components.themes import BOOTSTRAP
 
-from src.components.layout import create_layout
+from src.components.layout import LayoutBuilder
 from src.data.loader import load_transaction_data
+from src.data.source import DataSource
 
+LOCALE = 'pt'
 DATA_PATH = './data/transactions.csv'
 
 
 def main() -> None:
-    data = load_transaction_data(DATA_PATH)
+    i18n.set('locale', LOCALE)
+    i18n.load_path.append('locale')
+    data = load_transaction_data(DATA_PATH, locale=LOCALE)
+    data = DataSource(data)
 
     app = Dash(external_stylesheets=[BOOTSTRAP])
-    app.title = "Financial dashboard"
-    app.layout = create_layout(app, data)
+    app.title = i18n.t("general.app_title")
+    layout_builder = LayoutBuilder(app, data, LOCALE)
+    app.layout = layout_builder.create_layout()
     app.run()
 
 
